5016de55038450f4980d3cd241a1e101ba7d91b7
scottish_lacan <tyringebarn@yahoo.se>
2015-12-03 15:46:56 +0100

    Arnes kamera har förändrats
    Den är bättre nu - teleperspektiv och långt ifrån - det är bra blir bra
    kamera

--- Source/UNREALPROJEKT/UNREALPROJEKTCharacter.h
+++ Source/UNREALPROJEKT/UNREALPROJEKTCharacter.h
@@ -9,11 +9,11 @@ class AUNREALPROJEKTCharacter : public ACharacter
 	GENERATED_BODY()
 
 	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class USpringArmComponent* CameraBoom;
 
 	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
 public:
 	AUNREALPROJEKTCharacter();
