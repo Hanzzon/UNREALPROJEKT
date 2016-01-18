a786d5242cc2df432c1edcf933133f095acedc09
scottish_lacan <tyringebarn@yahoo.se>
2016-01-18 13:30:38 +0100

    Nu ska Arne ha de bra egenskaperna i luften

--- Source/UNREALPROJEKT/UNREALPROJEKTCharacter.cpp
+++ Source/UNREALPROJEKT/UNREALPROJEKTCharacter.cpp
@@ -24,7 +24,7 @@ AUNREALPROJEKTCharacter::AUNREALPROJEKTCharacter()
 	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
 	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 900.f;
-	GetCharacterMovement()->GravityScale = 1.2f;
+	GetCharacterMovement()->GravityScale = 1.2;
 	GetCharacterMovement()->AirControl = 0.5f;
 	GetCharacterMovement()->MaxWalkSpeed = 1000;
 
